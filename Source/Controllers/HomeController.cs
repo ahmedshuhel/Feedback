dc4c312e0514a94f5ddfa7a8980082a949425173
unknown <Shuhel Ahmed@darkhorse.(none)>
2013-12-18 21:36:49 +0600

    Organization search with typeahead hogan ghostwriter

--- Source/Controllers/HomeController.cs
+++ Source/Controllers/HomeController.cs
@@ -15,17 +15,8 @@ namespace ComplaintBox.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult SearchOrganizations()
         {
-            ViewBag.Message = "Your app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
