3a72668478f6e1e1b7a7b8bb36f00122ad63ce05
Shuhel Ahmed <shuhel@ymail.com>
2013-12-27 22:24:43 +0600

    fixed main page with main logo

---
+++ Source/Scripts/js/common.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    // Login form
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
+    
+    // strar rating
+    $(".rating > span").hover(function () {
+        $(this).removeClass();
+        $(this).addClass('glyphicon glyphicon-star');
+    });
+});
