b9c3ee45db8272c03f12cedfc8c9b9c235206015
Shuhel Ahmed <shuhel@ymail.com>
2013-12-28 22:28:21 +0600

    added admin template

---
+++ Source/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ComplaintBox.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
