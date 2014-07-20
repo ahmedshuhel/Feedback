cf0e7a8046b382aec3bcf8d06622042c27b4f751
Shuhel Ahmed <shuhel@ymail.com>
2014-07-20 21:55:07 +0600

    advanced feedback filter

--- Source/Controllers/AdminController.cs
+++ Source/Controllers/AdminController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using AutoMapper;
 using ComplaintBox.Web.Models;
-using System.Linq;
-using System.Data.Entity;
 using MvcPaging;
 
 namespace ComplaintBox.Web.Controllers
@@ -14,10 +14,16 @@ namespace ComplaintBox.Web.Controllers
     {
         private const int PageSize = 10;
 
+        private readonly string[] _months = new[]
+            {
+                "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+                "November", "December"
+            };
+
         public ActionResult Index()
         {
             string name = User.Identity.Name;
-            var model = GetAdminViewModel(name);
+            AdminViewModel model = GetAdminViewModel(name);
             return View(model);
         }
 
@@ -33,18 +39,18 @@ namespace ComplaintBox.Web.Controllers
             {
                 org = db.Organization.First(o => o.UserName == userName);
                 numberOfPendingTopics = db.Complaints.Count(c => c.Status == "NEW" && c.OrganizationId == org.Id);
-                numberOfResolvedFeedbacks = db.Complaints.Count(c => c.Status == "RESOLVED" && c.OrganizationId == org.Id);
+                numberOfResolvedFeedbacks =
+                    db.Complaints.Count(c => c.Status == "RESOLVED" && c.OrganizationId == org.Id);
                 settings = db.Settings.FirstOrDefault(s => s.OrganizationId == org.Id);
             }
 
-            var model = new AdminViewModel()
+            var model = new AdminViewModel
                 {
                     UserName = string.IsNullOrEmpty(org.FullName) ? userName : org.FullName,
                     NumberOfPendingTopics = numberOfPendingTopics,
                     NumbeOfResolvedFeedbacksToday = numberOfResolvedFeedbacks,
                     OrganizationId = org.Id,
                     OrganizationStatus = org.Status
-
                 };
 
             if (settings != null)
@@ -61,10 +67,9 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult SetSubjectTitle()
         {
-
             string name = User.Identity.Name;
 
-            var adminViewModel = GetAdminViewModel(name);
+            AdminViewModel adminViewModel = GetAdminViewModel(name);
 
             Settings settings;
             using (var db = new CboxContext())
@@ -72,7 +77,7 @@ namespace ComplaintBox.Web.Controllers
                 settings = db.Settings.FirstOrDefault(s => s.OrganizationId == adminViewModel.OrganizationId);
             }
 
-            var subjectTitle = Mapper.DynamicMap<AdminViewModel, SetSubjectTitle>(adminViewModel);
+            SetSubjectTitle subjectTitle = Mapper.DynamicMap<AdminViewModel, SetSubjectTitle>(adminViewModel);
 
             if (settings != null)
             {
@@ -85,16 +90,15 @@ namespace ComplaintBox.Web.Controllers
         [HttpPost]
         public ActionResult SetSubjectTitle(SetSubjectTitle model)
         {
-
             string name = User.Identity.Name;
-            var adminViewModel = GetAdminViewModel(name);
-            var subjectTitle = Mapper.DynamicMap<AdminViewModel, SetSubjectTitle>(adminViewModel);
+            AdminViewModel adminViewModel = GetAdminViewModel(name);
+            SetSubjectTitle subjectTitle = Mapper.DynamicMap<AdminViewModel, SetSubjectTitle>(adminViewModel);
             subjectTitle.NewSubjectTitle = model.NewSubjectTitle;
 
 
             using (var db = new CboxContext())
             {
-                var settings = db.Settings.FirstOrDefault(s => s.OrganizationId == adminViewModel.OrganizationId);
+                Settings settings = db.Settings.FirstOrDefault(s => s.OrganizationId == adminViewModel.OrganizationId);
 
                 if (settings == null)
                 {
@@ -121,7 +125,7 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult ListTopic()
         {
-            var adminViewModel = GetAdminViewModel(User.Identity.Name);
+            AdminViewModel adminViewModel = GetAdminViewModel(User.Identity.Name);
 
             ListTopicViewModel model = Mapper.DynamicMap<AdminViewModel, ListTopicViewModel>(adminViewModel);
 
@@ -133,7 +137,7 @@ namespace ComplaintBox.Web.Controllers
                 subjects = db.Subjects.Where(s => s.OrganizationId == adminViewModel.OrganizationId).ToList();
             }
 
-            model.Topics = subjects.Select(s => new Topic()
+            model.Topics = subjects.Select(s => new Topic
                 {
                     TopicId = s.Id,
                     TopicTitle = s.Title,
@@ -147,10 +151,9 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult AddNewTopic()
         {
-
             string name = User.Identity.Name;
-            var adminVm = GetAdminViewModel(name);
-            var newTopic = Mapper.DynamicMap<AdminViewModel, NewTopicViewModel>(adminVm);
+            AdminViewModel adminVm = GetAdminViewModel(name);
+            NewTopicViewModel newTopic = Mapper.DynamicMap<AdminViewModel, NewTopicViewModel>(adminVm);
 
             newTopic.TopicTitle = "";
             newTopic.OrgId = adminVm.OrganizationId;
@@ -161,10 +164,9 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult EditTopic(int id)
         {
-
             string name = User.Identity.Name;
-            var adminVm = GetAdminViewModel(name);
-            var topic = Mapper.DynamicMap<AdminViewModel, EditTopicViewModel>(adminVm);
+            AdminViewModel adminVm = GetAdminViewModel(name);
+            EditTopicViewModel topic = Mapper.DynamicMap<AdminViewModel, EditTopicViewModel>(adminVm);
 
             Subject subject;
             using (var db = new CboxContext())
@@ -183,7 +185,6 @@ namespace ComplaintBox.Web.Controllers
         [HttpPost]
         public ActionResult EditTopic(EditTopicViewModel model)
         {
-
             using (var db = new CboxContext())
             {
                 Subject subject = db.Subjects.Find(model.TopicId);
@@ -196,7 +197,6 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult DeleteTopic(int id)
         {
-
             using (var db = new CboxContext())
             {
                 Subject subject = db.Subjects.Find(id);
@@ -210,10 +210,9 @@ namespace ComplaintBox.Web.Controllers
         [HttpPost]
         public ActionResult AddNewTopic(NewTopicViewModel model)
         {
-
             using (var db = new CboxContext())
             {
-                db.Subjects.Add(new Subject()
+                db.Subjects.Add(new Subject
                     {
                         Title = model.TopicTitle,
                         OrganizationId = model.OrgId
@@ -231,7 +230,7 @@ namespace ComplaintBox.Web.Controllers
         {
             using (var db = new CboxContext())
             {
-                var org = db.Organization.Find(vm.Id);
+                Organization org = db.Organization.Find(vm.Id);
                 org.FullName = vm.OrganizationName;
                 org.PhoneNumber = vm.PhoneNumber;
                 org.Address = vm.Address;
@@ -246,12 +245,12 @@ namespace ComplaintBox.Web.Controllers
         [HttpGet]
         public ActionResult OrganizationDetails()
         {
-            var adminVm = GetAdminViewModel(User.Identity.Name);
-            var model = Mapper.DynamicMap<AdminViewModel, OrganizationDetailViewModel>(adminVm);
+            AdminViewModel adminVm = GetAdminViewModel(User.Identity.Name);
+            OrganizationDetailViewModel model = Mapper.DynamicMap<AdminViewModel, OrganizationDetailViewModel>(adminVm);
 
             var db = new CboxContext();
 
-            var organization = db.Organization.Find(adminVm.OrganizationId);
+            Organization organization = db.Organization.Find(adminVm.OrganizationId);
 
 
             model.Id = model.OrganizationId;
@@ -260,27 +259,24 @@ namespace ComplaintBox.Web.Controllers
             model.Address = organization.Address;
 
 
-
             return View(model);
         }
 
 
-
-        private FeedbackListViewModel GetFeedbackList(string status, int? page, int? topicId = null,  DateTime? fromDate = null, DateTime? toDate = null)
+        private FeedbackListViewModel GetFeedbackList(string status, int? page, int? topicId = null,
+                                                      DateTime? fromDate = null, DateTime? toDate = null)
         {
-
             int currentPageIndex = page.HasValue ? page.Value : 1;
 
-            var viewModel = GetAdminViewModel(User.Identity.Name);
-            var model = Mapper.DynamicMap<AdminViewModel, FeedbackListViewModel>(viewModel);
+            AdminViewModel viewModel = GetAdminViewModel(User.Identity.Name);
+            FeedbackListViewModel model = Mapper.DynamicMap<AdminViewModel, FeedbackListViewModel>(viewModel);
 
             List<FeedBackViewModel> feedbacks;
 
             using (var db = new CboxContext())
             {
-
-                var query = db.Complaints.Where(c => c.OrganizationId == viewModel.OrganizationId)
-                              .Include(c => c.Subject);
+                IQueryable<Complaint> query = db.Complaints.Where(c => c.OrganizationId == viewModel.OrganizationId)
+                                                .Include(c => c.Subject);
 
                 if (status == "NEW")
                 {
@@ -293,30 +289,29 @@ namespace ComplaintBox.Web.Controllers
 
                 if (fromDate != null && toDate != null)
                 {
-                    query = query.Where(c => c.ComplainDate >= fromDate && c.ComplainDate < toDate);    
+                    query = query.Where(c => c.ComplainDate >= fromDate && c.ComplainDate < toDate);
                 }
 
 
-                if (topicId != null)
+                if (topicId != null && topicId > 0)
                 {
                     query = query.Where(c => c.SubjectId == topicId);
                 }
 
 
-
-                feedbacks =   query.Where(c=> c.OrganizationId == viewModel.OrganizationId)
-                              .OrderBy(c=> c.ComplainDate)
-                              .AsEnumerable()
-                              .Select(c => new FeedBackViewModel()
-                                  {
-                                      Email = c.EmailAddress,
-                                      FeedbackUser = c.Complainer,
-                                      FeedbackDate = c.ComplainDate,
-                                      Feedback = c.Description,
-                                      Topic = c.Subject == null ? "" : c.Subject.Title,
-                                      FeedbackId = c.Id,
-                                      Status = c.Status
-                                  }).ToList();
+                feedbacks = query.Where(c => c.OrganizationId == viewModel.OrganizationId)
+                                 .OrderBy(c => c.ComplainDate)
+                                 .AsEnumerable()
+                                 .Select(c => new FeedBackViewModel
+                                     {
+                                         Email = c.EmailAddress,
+                                         FeedbackUser = c.Complainer,
+                                         FeedbackDate = c.ComplainDate,
+                                         Feedback = c.Description,
+                                         Topic = c.Subject == null ? "" : c.Subject.Title,
+                                         FeedbackId = c.Id,
+                                         Status = c.Status
+                                     }).ToList();
             }
 
 
@@ -329,41 +324,127 @@ namespace ComplaintBox.Web.Controllers
         {
             ViewBag.FeedbackListTitle = "Recent Feedback List";
 
-            var model = GetFeedbackList("ALL", page);
+            FeedbackListViewModel model = GetFeedbackList("ALL", page);
             return View(model);
         }
 
 
         public ActionResult TodaysFeedback(int? page)
         {
-
             ViewBag.FeedbackListTitle = "Today's Feedback List";
 
             DateTime? fromDate = DateTime.Today;
             DateTime? toDate = DateTime.Today.AddDays(1);
 
-            var model = GetFeedbackList("ALL", page, null, fromDate, toDate);
+            FeedbackListViewModel model = GetFeedbackList("ALL", page, null, fromDate, toDate);
             return View("FeedbackList", model);
         }
 
-        public ActionResult ResolvedFeedbackList(int? page)
+        public ActionResult WeeksFeedback(int? page)
+        {
+            DateTime? fromDate = DateTime.Today.AddDays(-7);
+            DateTime? toDate = DateTime.Today.AddDays(1);
+
+
+            ViewBag.FeedbackListTitle = "Feedbaks of Last Seven Days";
+
+            FeedbackListViewModel model = GetFeedbackList("ALL", page, null, fromDate, toDate);
+            return View("FeedbackList", model);
+        }
+
+
+        public ActionResult MonthsFeedback(int? page)
+        {
+            int month = DateTime.Today.Month;
+            int daysInMonth = DateTime.DaysInMonth(DateTime.Today.Year, month);
+
+            DateTime? fromDate = new DateTime(DateTime.Today.Year, month, 1);
+            DateTime? toDate = new DateTime(DateTime.Today.Year, month, daysInMonth);
+
+            ViewBag.FeedbackListTitle = "Feedback List of " + _months[month - 1] + ", " + DateTime.Today.Year.ToString();
+            FeedbackListViewModel model = GetFeedbackList("ALL", page, null, fromDate, toDate);
+            return View("FeedbackList", model);
+        }
+
+
+
+         public ActionResult AdvancedFeedbackFilter()
+         {
+             AdminViewModel adminViewModel = GetAdminViewModel(User.Identity.Name);
+             List<Subject> subjects;
+             using (var db = new CboxContext())
+             {
+                 subjects = db.Subjects.Where(s => s.OrganizationId == adminViewModel.OrganizationId).ToList();
+             }
+
+             IEnumerable<SelectListItem> single = new[]
+                {
+                    new SelectListItem()
+                        {
+                            Value = "-1",
+                            Text = "ALL"
+                        }, 
+                };
+
+             IEnumerable<SelectListItem> subjectList = subjects.Select(
+                 s => new SelectListItem
+                 {
+                     Text = s.Title,
+                     Value = s.Id.ToString()
+                 });
+
+
+
+             ViewBag.Subjects = single.Union(subjectList);
+
+             return View(adminViewModel);
+         }
+
+        
+        public ActionResult AdvancedFeedbackList(AdvancedFilter filter, int? page)
         {
 
-            var model = GetFeedbackList("RESOLVED", page);
+            ViewBag.TopicId = filter.TopicId;
+            ViewBag.FromDate = filter.FromDate;
+            ViewBag.ToDate = filter.ToDate;
+            ViewBag.Status = filter.Status;
+
+
+            if (filter.TopicId > 0)
+            {
+                Subject subject;
+                using (var db = new CboxContext())
+                {
+                    subject = db.Subjects.Find(filter.TopicId);
+                }
+                ViewBag.Topic = subject.Title;
+    
+            }
+            else
+            {
+                ViewBag.Topic = "ALL";
+            }
+            
+            var model = GetFeedbackList(filter.Status, page, filter.TopicId, filter.FromDate, filter.ToDate);
+            return View(model);
+        }
+
+
+        public ActionResult ResolvedFeedbackList(int? page)
+        {
+            FeedbackListViewModel model = GetFeedbackList("RESOLVED", page);
             return View(model);
         }
 
         public ActionResult NewFeedbackList(int? page)
         {
-            var model = GetFeedbackList("NEW", page);
+            FeedbackListViewModel model = GetFeedbackList("NEW", page);
             return View(model);
         }
 
         public ActionResult FeedbackDetails(int id)
         {
             var admivVm = GetAdminViewModel(User.Identity.Name);
-            
-            
             var model = Mapper.DynamicMap<AdminViewModel, FeedbackDetailsViewModel>(admivVm);
 
             Complaint complaint;
@@ -387,10 +468,9 @@ namespace ComplaintBox.Web.Controllers
 
         public ActionResult ResolveFeedback(FeedbackDetailsViewModel vm)
         {
-
             using (var db = new CboxContext())
             {
-               var  complaint = db.Complaints.Find(vm.FeedbackId);
+                Complaint complaint = db.Complaints.Find(vm.FeedbackId);
                 complaint.Comment = vm.Comment;
                 complaint.Status = "RESOLVED";
                 db.SaveChanges();
@@ -404,7 +484,7 @@ namespace ComplaintBox.Web.Controllers
         {
             using (var db = new CboxContext())
             {
-                var organization = db.Organization.Find(id);
+                Organization organization = db.Organization.Find(id);
                 organization.Status = OrganizationStatus.New;
                 db.SaveChanges();
             }
@@ -423,7 +503,5 @@ namespace ComplaintBox.Web.Controllers
 
             return RedirectToAction("Index");
         }
-
-
     }
 }
---
+++ Source/Models/AdvancedFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComplaintBox.Web.Models
+{
+    public class AdvancedFilter
+    {
+        public int TopicId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public string Status { get; set; }
+    }
+}
